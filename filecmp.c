feb2c965f8f851381bb7b4581fed28761d7a7212
Valerie Martinez <47126912+valmartnz@users.noreply.github.com>
2020-05-03 19:14:47 -0700

    Add files via upload

---
+++ filecmp.c
@@ -0,0 +1,44 @@
+// --------------- 7.6 ---------------
+// compare two files, printing the first line where they differ
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void filecmp(FILE *fin1, FILE *fin2) {
+  int f1, f2;
+  while (1) {
+    if ((f1 = fgetc(fin1)) == EOF)
+      break;
+    if ((f2 = fgetc(fin2)) == EOF)
+      break;
+
+    if (f1 != f2) {
+    putchar(f1);
+    putchar(f2);
+    break;
+    }
+  }
+}
+
+int main(int argc, const char *argv[]) {
+  FILE *fin1, *fin2;
+
+  if(argc == 1) { fprintf(stderr, "Usage: ./filecmp file1 file2\n");  exit(1); }
+
+  while (--argc > 0) {
+    if ((fin1 = fopen(argv[1], "r")) == NULL) {
+      printf("can't open first file\n");  exit(1);
+    }
+
+    if ((fin2 = fopen(argv[1], "r")) == NULL) {
+      printf("can't open second file\n");  exit(1);
+    }
+
+    filecmp(fin1, fin2);
+    fclose(fin1);
+    fclose(fin2);
+  }
+  return 0;
+}
---
+++ lower_and_upper.c
@@ -0,0 +1,28 @@
+// --------------- 7.1 ---------------
+// converts upper case to lower or lower case to upper
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, const char *argv[]) {
+  int c;
+
+  if(argc != 2) { fprintf(stderr, "Usage: ./(lower/upper) filename\n");  exit(1); }
+  FILE *fin = fopen(argv[1], "r");
+
+  if (strcmp(argv[0], "./lower") == 0) {
+    while((c = fgetc(fin)) != EOF) {
+      putchar(tolower(c));
+    }
+    printf("\n");
+  }
+  else {
+    while((c = fgetc(fin)) != EOF) {
+      putchar(toupper(c));
+    }
+    printf("\n");
+  }
+  return 0;
+}
